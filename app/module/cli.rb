672d11a8bccabe0b8bb30b061af87723aada0975
vovka26 <317110@gmail.com>
2018-12-05 11:06:46 -0500

    changed seeds

--- app/module/cli.rb
+++ app/module/cli.rb
@@ -13,12 +13,9 @@ class Cli
             # if language == language_that_does_not_exists
             #   puts "We are apologize. We do not know this language yet. Please come back and check it out later. Or learn one of these languages"
             # show all languages that are available
-    end
+  
   end
 
-  def method_name
-
-  end
 
   def input
     gets.downcase.chomp
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
-Category.destroy_all
-Translation.destroy_all
-CategoriesTranslations.destroy_all
+# Category.destroy_all
+# Translation.destroy_all
+# CategoriesTranslations.destroy_all
+
+greetings = Category.create(categories_translations: "Greetings")
+food = Category.create(categories_translations: "Food")
+directions = Category.create(categories_translations: "Directions")
+
 
 hello = Translation.create(english: "Hello", russian: "Привет", arabic: "مرحبا", french: "Bonjour")
+
+
+hello_translation = CategoriesTranslations.find_or_create_by(hello: hello, category: greetings)
