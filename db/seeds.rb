9e09208f0e3c1b20023ef621f6006e35c79a813c
vovka26 <317110@gmail.com>
2018-12-05 09:19:49 -0500

    added destroy_all for all tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
+Category.destroy_all
+Translation.destroy_all
+CategoriesTranslations.destroy_all
+
 hello = Translation.create(english: "Hello", russian: "Привет", arabic: "مرحبا", french: "Bonjour")
