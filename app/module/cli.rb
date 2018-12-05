423d8ad2503b47f51f9e4674ce20e77891d4bcd2
vovka26 <317110@gmail.com>
2018-12-05 14:39:23 -0500

    connected database to cli

--- app/module/cli.rb
+++ app/module/cli.rb
@@ -5,8 +5,29 @@ class Cli
     puts "Welcome to PROGRAM_NAME. What is your name?"
     name = self.input
     puts "Thank you, #{name.capitalize}. What language would you like to learn today?"
-    language = input
-    puts "Great! #{language.capitalize} it is!"
+    language_choice = input
+    puts "Great! #{language_choice.capitalize} it is!"
+    puts "What category would you like to practice today?"
+    category_choice = input.capitalize.pluralize
+    puts "Great choice! Here are your words."
+
+    category_choice_id = Category.where(name: category_choice).pluck(:id).first
+
+    translation_choice_id = CategoriesTranslation.where(category_id: category_choice_id).pluck(:translation_id)
+
+    translation_row = Translation.where(id: translation_choice_id)
+
+    translation_row.collect do |row|
+      puts "#{row.english} => #{row.send(language_choice)}"
+    end
+
+
+    # binding.pry
+    # puts translation_row.language_choice
+
+
+
+    # puts category_choice_id.inspect
             # if language == french
             #   puts "Great #{language} it is!" in french
             # end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,18 @@ Category.destroy_all
 Translation.destroy_all
 CategoriesTranslation.destroy_all
 
-greeting = Category.find_or_create_by(name: "Greetings")
-food = Category.find_or_create_by(name: "Food")
+greetings = Category.find_or_create_by(name: "Greetings")
+foods = Category.find_or_create_by(name: "Foods")
 directions = Category.find_or_create_by(name: "Directions")
 
 
 hello = Translation.find_or_create_by(english: "Hello", russian: "Привет", arabic: "مرحبا", french: "Bonjour")
+bye = Translation.find_or_create_by(english: "Bye", russian: "Пока", arabic: "ما سلامة", french: "Au revoir")
+how_are_you = Translation.find_or_create_by(english: "How are you?", russian: "Как ваши дела?", arabic: "كيف الحال؟", french: "comment ça va?")
+im_well_and_you = Translation.find_or_create_by(english: "I am well, and you?", arabic: "انا بخير وأنت", french: "ça va bien et vous?", russian: "Я хорошо. Как Ваши дела?")
 
 
-hello_translation = CategoriesTranslation.create!(category: greeting, translation: hello)
+hello_translation = CategoriesTranslation.create!(category: greetings, translation: hello)
+bye_translation = CategoriesTranslation.create(category: greetings, translation: bye)
+how_are_you_translation = CategoriesTranslation.create(category: greetings, translation: how_are_you)
+im_well_and_you_translation = CategoriesTranslation.create(category: greetings, translation: im_well_and_you)
