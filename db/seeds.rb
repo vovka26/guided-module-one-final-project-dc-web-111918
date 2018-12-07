a6df8f1ec34aeeeb34d625d035129110729a4602
vovka26 <317110@gmail.com>
2018-12-07 00:05:29 -0500

    updated seeds. fixed the problem with design :D

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,42 +8,41 @@ directions = Category.find_or_create_by(name: "Directions")
 
   # GREETINGS
 
-hello = Translation.find_or_create_by(english: "Hello", russian: "Привет", arabic: "مرحبا", french: "Bonjour")
+  hello = Translation.find_or_create_by(english: "Hello", russian: "Привет", arabic: "مرحبا marhaba", french: "Bonjour")
 
-  ######
-good_morning = Translation.find_or_create_by(english: "Good morning", russian: "Доброе утро", arabic: "", french: "")
-good_night = Translation.find_or_create_by(english: "Good night", russian: "Спокойной ночи", arabic: "", french: "")
-good_evening = Translation.find_or_create_by(english: "Good evening", russian: "Доброе утро", arabic: "", french: "")
-nice_to_meet_you = Translation.find_or_create_by(english: "Nice to meet you", russian: "Приятно познакомиться", arabic: "", french: "")
+    ######
+  good_morning = Translation.find_or_create_by(english: "Good morning", russian: "Доброе утро", arabic: "صباح الخير sabah al-khyr", french: "Bonjour (also means hello)")
+  good_night = Translation.find_or_create_by(english: "Good night", russian: "Спокойной ночи", arabic:"تصبح على خير - tsbih 3alaa khayr", french: "Bonne nuit")
+  good_evening = Translation.find_or_create_by(english: "Good evening", russian: "Доброе утро", arabic: "مساء الخير  masa' al-khayr", french: "Bonsoir")
+  nice_to_meet_you = Translation.find_or_create_by(english: "Nice to meet you", russian: "Приятно познакомиться", arabic: "تشرّفنا tasharrafna", french: "enchanté")
 
-  ######
+    ######
 
-bye = Translation.find_or_create_by(english: "Bye", russian: "Пока", arabic: "ما سلامة", french: "Au revoir")
-how_are_you = Translation.find_or_create_by(english: "How are you?", russian: "Как ваши дела?", arabic: "كيف الحال؟", french: "comment ça va?")
-im_well_and_you = Translation.find_or_create_by(english: "I am well, and you?", arabic: "انا بخير وأنت", french: "ça va bien et vous?", russian: "Я хорошо. Как Ваши дела?")
+  bye = Translation.find_or_create_by(english: "Bye", russian: "Пока", arabic: "ما سلامة maa salaama", french: "Au revoir")
+  how_are_you = Translation.find_or_create_by(english: "How are you?", russian: "Как ваши дела?", arabic: "كيف الحال؟ kiif al-Haal", french: "comment ça va?")
+  im_well_and_you = Translation.find_or_create_by(english: "I am well, and you?", arabic: "انا بخير وأنت anaa bikhayr wa inta/i (m/f)", french: "ça va bien et vous?", russian: "Я хорошо. Как Ваши дела?")
 
-  #FOODS
+    #FOODS
 
-breakfast = Translation.find_or_create_by(english: "Breakfast", russian: "Завтрак", arabic: "", french: "")
-lunch = Translation.find_or_create_by(english: "Lunch", russian: "Обед", arabic: "", french: "")
-dinner = Translation.find_or_create_by(english: "Dinner", russian: "Ужин", arabic: "", french: "")
-enjoy_your_meal = Translation.find_or_create_by(english: "Enjoy your meal", russian: "Приятного аппетита", arabic: "", french: "")
-eat = Translation.find_or_create_by(english: "Eat", russian: "Кушать", arabic: "", french: "")
-chew = Translation.find_or_create_by(english: "Chew", russian: "Жевать", arabic: "", french: "")
-apple = Translation.find_or_create_by(english: "Apple", russian: "Яблоко", arabic: "", french: "")
-orange = Translation.find_or_create_by(english: "Orange", russian: "Апельсин", arabic: "", french: "")
+  breakfast = Translation.find_or_create_by(english: "Breakfast", russian: "Завтрак", arabic: "الفطورal-futoor", french: "petit déjeuner")
+  lunch = Translation.find_or_create_by(english: "Lunch", russian: "Обед", arabic: "الغداء al-ghadaa'", french: "le déjeuner/ le repas de midi")
+  dinner = Translation.find_or_create_by(english: "Dinner", russian: "Ужин", arabic: "العشاءal-3ashaa'", french: "le dîner / le repas du soir")
+  enjoy_your_meal = Translation.find_or_create_by(english: "Enjoy your meal", russian: "Приятного аппетита", arabic: " صحة Sa7a (literally means health)", french: "bon appétit")
+  eat = Translation.find_or_create_by(english: "Eat", russian: "Кушать", arabic: "أكل-akil", french: "manger")
+  chew = Translation.find_or_create_by(english: "Chew", russian: "Жевать", arabic: "مَضْغ maDgh", french: "mâcher")
+  apple = Translation.find_or_create_by(english: "Apple", russian: "Яблоко", arabic: "تُفَّاحَة tufaaHa", french: "Pomme")
+  orange = Translation.find_or_create_by(english: "Orange", russian: "Апельсин", arabic: "برتقال burtuQal", french: "orange")
 
- ############
+   ############
 
-  #DIRECTIONS
+    #DIRECTIONS
 
-left = Translation.find_or_create_by(english: "Left", russian: "Лево", arabic: "", french: "")
-right = Translation.find_or_create_by(english: "Right", russian: "Право", arabic: "", french: "")
-up = Translation.find_or_create_by(english: "Up", russian: "Вверх", arabic: "", french: "")
-down = Translation.find_or_create_by(english: "Down", russian: "Вниз", arabic: "", french: "")
-how_do_i_get_to = Translation.find_or_create_by(english: "How do I get to...", russian: "Как пройти к...?", arabic: "", french: "")
-where = Translation.find_or_create_by(english: "Where", russian: "Где", arabic: "", french: "")
-
+  left = Translation.find_or_create_by(english: "Left", russian: "Лево", arabic: "اليسارal-yisaar", french: "gauche")
+  right = Translation.find_or_create_by(english: "Right", russian: "Право", arabic: "اليمين al-yamiin", french: "droite")
+  up = Translation.find_or_create_by(english: "Up", russian: "Вверх", arabic: "إلى فوقilaa fauuQ", french: "en haut")
+  down = Translation.find_or_create_by(english: "Down", russian: "Вниз", arabic: "DOUBLE CHECK", french: "en bas")
+  how_do_i_get_to = Translation.find_or_create_by(english: "How do I get to...", russian: "Как пройти к...?", arabic: "ما هو الطريق ma huwa a-TariiQ", french: "Comment puis-je aller à")
+  where = Translation.find_or_create_by(english: "Where", russian: "Где", arabic: "أين؟-ayna", french: "où?")
  ############
 
 hello_translation = CategoriesTranslation.create!(category: greetings, translation: hello)
