707fc5065d63638d8beb63e416734596fad77163
Manon Pilaud <mandyypilaud@gmail.com>
2018-12-07 11:42:32 -0500

    created new categories in seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,30 @@ CategoriesTranslation.destroy_all
 greetings = Category.find_or_create_by(name: "Greetings")
 foods = Category.find_or_create_by(name: "Foods")
 directions = Category.find_or_create_by(name: "Directions")
+politeness = Category.find_or_create_by(name: "Politeness")
+the_family = Category.find_or_create_by(name: "Family")
 
+  #Family
+  family = Translation.find_or_create_by(english: "family", russian: "", arabic: "أسرة usra (nuclear not extended)", french: "famille")
+  father = Translation.find_or_create_by(english: "father", russian: "", arabic: "والد waaled", french: "père")
+  mother = Translation.find_or_create_by(english: "mother", russian: "", arabic: "والدة waaleda", french: "mère")
+  sister = Translation.find_or_create_by(english: "sister", russian: "", arabic: "أخت ukht", french: "sœur")
+  brother = Translation.find_or_create_by(english: "brother", russian: "", arabic: "أخ akh", french: "frère")
+  son = Translation.find_or_create_by(english: "son", russian: "", arabic: "ابن ibn", french: "fils")
+  daughter = Translation.find_or_create_by(english: "daughter", russian: "", arabic: "بنت bint (girl)", french: "fille")
+  husband = Translation.find_or_create_by(english:"husband", russian: "", arabic: "زوج zauuj", french: "mari")
+  wife = Translation.find_or_create_by(english: "wife", russian: "", arabic: "زوجة zauuja", french: "femme")
+  grandmother = Translation.find_or_create_by(english: "grandmother", russian: "", arabic: "جد jad", french: "grand-mère")
+  grandfather = Translation.find_or_create_by(english: "grandfather", russian: "", arabic: "جدة jada", french: "grand-père")
+
+  #POLITENESS
+  thank_you = Translation.find_or_create_by(english: "Thank you", russian: "", arabic: "شكرا shukraan", french: "Merci")
+  you_are_welcome = Translation.find_or_create_by(english:"You're welcome", russian: "", arabic: "عفوا a3fwaan ", french: "de rien")
+  welcome = Translation.find_or_create_by(english: "Welcome", russian: "", arabic:"أهلا وسهلا- ahlaan wa sahlaan (hello welcome)", french: "Bienvenue")
+  please = Translation.find_or_create_by(english: "Please", russian: "", arabic: "من فضلَك/من فضلِك min faDlak/fadlik m/f", french: "S'il vous plaît(formal), S'il te plaît (informal)")
+  excuse_me = Translation.find_or_create_by(english: "Excuse me", russian: "", arabic: "عفوا a3fwaan", french: "Excusez-moi")
+  sorry = Translation.find_or_create_by(english: "Sorry", russian: "", arabic: "آسف/ة assif/assifa m/f", french: "Pardon/désolé")
+  come_in = Translation.find_or_create_by(english: "come in", russian: "", arabic: "تفضل tafaDDal/i m/f", french: "ِEntrer")
   # GREETINGS
 
   hello = Translation.find_or_create_by(english: "Hello", russian: "Привет", arabic: "مرحبا marhaba", french: "Bonjour")
@@ -16,9 +39,8 @@ directions = Category.find_or_create_by(name: "Directions")
   bye = Translation.find_or_create_by(english: "Bye", russian: "Пока", arabic: "ما سلامة maa salaama", french: "Au revoir")
   how_are_you = Translation.find_or_create_by(english: "How are you?", russian: "Как ваши дела?", arabic: "كيف الحال؟ kiif al-Haal", french: "comment ça va?")
   im_well_and_you = Translation.find_or_create_by(english: "I am well, and you?", arabic: "انا بخير وأنت anaa bikhayr wa inta/i (m/f)", french: "ça va bien et vous?", russian: "Я хорошо. Как Ваши дела?")
-
   see_you_soon = Translation.find_or_create_by(english: "see you soon", arabic: "اراك قريبا araka Qareebaan", french:"à bientôt", russian: "")
-  what_is_new= Translation.find_or_create_by(english: "What is new?", arabic: "ما الاخبار maa al-akhbar or shoo akhbar", french: "Quoi de neuf?", russian: "")
+  what_is_new= Translation.find_or_create_by(english: "What is new?", arabic: "ما الاخبار maa al-akhbar or shoo akhbar(dialect)", french: "Quoi de neuf?", russian: "")
     #FOODS
 
   breakfast = Translation.find_or_create_by(english: "Breakfast", russian: "Завтрак", arabic: "الفطورal-futoor", french: "petit déjeuner")
@@ -29,7 +51,6 @@ directions = Category.find_or_create_by(name: "Directions")
   chew = Translation.find_or_create_by(english: "Chew", russian: "Жевать", arabic: "مَضْغ maDgh", french: "mâcher")
   apple = Translation.find_or_create_by(english: "Apple", russian: "Яблоко", arabic: "تُفَّاحَة tufaaHa", french: "Pomme")
   orange = Translation.find_or_create_by(english: "Orange", russian: "Апельсин", arabic: "برتقال burtuQal", french: "orange")
-
   to_drink = Translation.find_or_create_by(english: "to_drink", russian: "", arabic: "شرب shurb", french: "boire")
   water =  Translation.find_or_create_by(english: "water", russian: "", arabic: "ماء maa'", french: "Eau")
   ############
@@ -42,7 +63,6 @@ directions = Category.find_or_create_by(name: "Directions")
   down = Translation.find_or_create_by(english: "Down", russian: "Вниз", arabic: "DOUBLE CHECK", french: "en bas")
   how_do_i_get_to = Translation.find_or_create_by(english: "How do I get to...", russian: "Как пройти к...?", arabic: "ما هو الطريق ma huwa a-TariiQ", french: "Comment puis-je aller à")
   where = Translation.find_or_create_by(english: "Where", russian: "Где", arabic: "أين؟-ayna", french: "où?")
-
   city = Translation.find_or_create_by(english: "City", russian: "", arabic: "مدينة madiina", french: "Ville")
   hotel = Translation.find_or_create_by(english: "Hotel", russian: "", arabic: "الفندقal-fonduQ", french: "hôtel")
   car = Translation.find_or_create_by(english: "Car", russian: "", arabic: "سيارة sayaara", french: "Voiture")
@@ -72,15 +92,32 @@ up_translation = CategoriesTranslation.create(category: directions, translation:
 down_translation = CategoriesTranslation.create(category: directions, translation: down)
 how_do_i_get_to_translation = CategoriesTranslation.create(category: directions, translation: how_do_i_get_to)
 where_translation = CategoriesTranslation.create(category: directions, translation: where)
-
 city = CategoriesTranslation.create(category: directions, translation: city)
 hotel = CategoriesTranslation.create(category: directions, translation: hotel)
 car = CategoriesTranslation.create(category: directions, translation: car)
 plane = CategoriesTranslation.create(category: directions, translation: plane)
 the_road = CategoriesTranslation.create(category: directions, translation:the_road)
-
 to_drink = CategoriesTranslation.create(category: foods, translation: to_drink)
 water = CategoriesTranslation.create(category: foods, translation: water)
-
 see_you_soon = CategoriesTranslation.create(category: greetings, translation: see_you_soon)
 what_is_new= CategoriesTranslation.create(category: greetings, translation: what_is_new )
+
+thank_you = CategoriesTranslation.create(category:politeness , translation: thank_you)
+you_are_welcome = CategoriesTranslation.create(category:politeness , translation: you_are_welcome)
+welcome = CategoriesTranslation.create(category:politeness , translation: welcome)
+please = CategoriesTranslation.create(category:politeness, translation: please)
+excuse_me = CategoriesTranslation.create(category:politeness , translation: excuse_me)
+sorry = CategoriesTranslation.create(category:politeness , translation: sorry)
+come_in = CategoriesTranslation.create(category:politeness , translation: come_in)
+
+family = CategoriesTranslation.create(category:the_family, translation: family)
+father = CategoriesTranslation.create(category:the_family, translation: father)
+mother = CategoriesTranslation.create(category:the_family, translation: mother)
+sister = CategoriesTranslation.create(category:the_family, translation: sister)
+brother= CategoriesTranslation.create(category:the_family, translation: brother)
+son = CategoriesTranslation.create(category:the_family, translation: son)
+daughter = CategoriesTranslation.create(category:the_family, translation: daughter)
+husband = CategoriesTranslation.create(category:the_family, translation: husband)
+wife = CategoriesTranslation.create(category:the_family, translation: wife)
+grandmother = CategoriesTranslation.create(category:the_family, translation: grandmother)
+grandfather = CategoriesTranslation.create(category:the_family, translation: grandfather)
